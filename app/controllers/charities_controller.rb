496b7c5e74fabb1684668c67a6dd9221c11ed901
Tim Cecatka <tim.cecatka@gmail.com>
2020-11-24 15:51:16 +0100

    charity#show displays CharitiesChoices

--- app/controllers/charities_controller.rb
+++ app/controllers/charities_controller.rb
@@ -1,11 +1,12 @@
 class CharitiesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index, :show ]
 
-def index
-  @charities = Charity.all
-end
+  def index
+    @charities = Charity.all
+  end
 
-def show
-  @charity = Charity.find(params[:id])
-end
+  def show
+    @charity = Charity.find(params[:id])
+    @choices = CharitysChoice.where(charity_id: @charity.id)
+  end
 end
