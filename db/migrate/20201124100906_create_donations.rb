9dd0323c104acd2f5fe09e9ebd5a3039d4dd5f7f
roman vidal <roman.vidal@stud.uni-regensburg.de>
2020-11-24 15:44:54 +0100

    delete symbol attribute form donations

--- db/migrate/20201124100906_create_donations.rb
+++ db/migrate/20201124100906_create_donations.rb
@@ -6,7 +6,6 @@ class CreateDonations < ActiveRecord::Migration[6.0]
       t.references :asset, null: false, foreign_key: true
       t.integer :donation_amount
       t.integer :quantity
-      t.string :symbol
       t.string :order_type, :default =>"Market"
       t.integer :limit_price 
       t.integer :stop_price
