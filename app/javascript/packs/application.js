c634fe7f4f84cfc987cce712721830e77c95708d
roman vidal <roman.vidal@stud.uni-regensburg.de>
2020-12-01 16:01:12 +0100

    fix js calculation and add a photo to stripe checkout -> we should use bigger fotos in our seeds

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,7 +24,7 @@ require("channels")
 
 // External imports
 import "bootstrap";
-import { totalPrice } from "./totalprice";
+import { updateTotalPrice } from "./totalprice";
 import { scroll_input } from "./scroll";
 
 // Internal imports, e.g:
@@ -33,6 +33,6 @@ import { scroll_input } from "./scroll";
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
-  totalPrice;
+  updateTotalPrice();
   scroll_input();
 });
