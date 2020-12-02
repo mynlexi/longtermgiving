03076e5f97687e7e93ac5a713ca3ca7dd6d9c935
roman vidal <roman.vidal@stud.uni-regensburg.de>
2020-12-02 16:07:32 +0100

    delete CO summary page and fixed some js bugs from the application page

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -57,7 +57,7 @@ class DonationsController < ApplicationController
       )
       @donation.amount = @price * @quantity.to_i
       @donation.update(checkout_session_id: session.id)
-      redirect_to new_charity_donation_payment_path(@charity, @donation)
+      # redirect_to new_charity_donation_payment_path(@charity, @donation)
       # redirect_to dashboard_path
     else
       redirect_to charity_path(@charity)
--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,7 +1,6 @@
 class PaymentsController < ApplicationController
     def new
         @donation = current_user.donations.find(params[:donation_id])
-    
         authorize @donation
     end
 end
