8f8a563a86c39874669cd0f1efa951a8a9385ab4
Tim Cecatka <tim.cecatka@gmail.com>
2020-12-04 13:02:27 +0100

    banner rework

--- app/javascript/packs/banner.js
+++ app/javascript/packs/banner.js
@@ -2,7 +2,7 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
-    strings: ["the FUTURE", "GLD", "SPY", "QQQ", "AAPL", "TSLA", "CORN", "VGK", "VEA", "YOLO", "NEL.OL"],
+    strings: ["the FUTURE", "GLD", "SPY", "QQQ", "the FUTURE", "AAPL", "TSLA", "CORN", "VGK", "the FUTURE", "VEA", "YOLO", "NEL.OL"],
     typeSpeed: 200,
     loop: true,
   });
